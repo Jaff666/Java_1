5d9248296a91edee4dced1b35190871f9a5e635f
Jaff666 <75972621+Jaff666@users.noreply.github.com>
2021-08-29 16:19:58 +0200

    Add files via upload

---
+++ LAB_08/src/zad4/Main.java
@@ -0,0 +1,17 @@
+package zad4;
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0);
+        Diamond diamond = new Diamond(2.0,4.0);
+        Trapeze trapeze = new Trapeze(2.0, 4.0, 6.0, 8.0, 4.0);
+        
+        Figure[] array = new Figure[] { circle, diamond, trapeze };
+        
+        for (Figure figure : array) {
+            Arrays.sort(array);
+            System.out.println("Area: " + figure.calculateArea());
+        }
+
+    }
+}
