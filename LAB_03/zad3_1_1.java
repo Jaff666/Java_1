4acf6a78cf640e7523f50294f847a3b19e422650
Jaff666 <75972621+Jaff666@users.noreply.github.com>
2021-08-29 16:18:11 +0200

    Add files via upload

---
+++ LAB_03/zad3_1_1.java
@@ -0,0 +1,16 @@
+public class zad3_1_1 {
+    int bok;
+
+
+    public zad3_1_1(int bok) {
+        this.bok = bok;
+    }
+
+    public int obliczPole() {
+        return bok * bok;
+    }
+
+    public int obliczObwod() {
+        return bok*4;
+    }
+}
