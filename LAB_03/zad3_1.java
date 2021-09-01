36bef82c043078bd27bcaf918505c9090275b88f
Michal_K <75972621+Jaff666@users.noreply.github.com>
2021-09-01 18:05:23 +0200

    Delete LAB_03 directory

--- LAB_03/zad3_1.java
+++
@@ -1,7 +0,0 @@
-public class zad3_1 {
-    public static void main(String[] args)  {
-        zad3_1_1 kwadrat = new zad3_1_1(5);
-        System.out.println(kwadrat.obliczPole());
-        System.out.println(kwadrat.obliczObwod());
-    }
-}
--- LAB_03/zad3_2.java
+++
@@ -1,3 +0,0 @@
-public class zad3_2 {
-    
-}
