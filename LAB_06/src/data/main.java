da630e93f8da4cfa96a04e16ffa0a374e91abcb3
Michal_K <75972621+M-Kalinowski2001@users.noreply.github.com>
2021-09-02 11:32:35 +0200

    Delete LAB_06 directory

--- LAB_06/src/data/main.java
+++
@@ -1,9 +0,0 @@
-package data;
-
-public class main {
-    public static void main(String[] args){
-        MojaData pozycja0 = new MojaData(10,11,2001);
-        System.out.println(pozycja0);
-    }
-
-}
