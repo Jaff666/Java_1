8336e0926156f875c526be4fe85b46ccd0c44d3c
Michal_K <75972621+M-Kalinowski2001@users.noreply.github.com>
2021-09-02 11:32:29 +0200

    Delete LAB_05 directory

--- LAB_05/src/calculator/MainCalculator.java
+++
@@ -1,9 +0,0 @@
-package calculator;
-public class MainCalculator {
-    public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        System.out.println(calculator.Calculate(4));
-        System.out.println(calculator.Calculate(4,15));
-        System.out.println(calculator.Calculate(1,6,7));
-    }
-}
